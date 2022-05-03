0d9a49aa9cc50d4b84a63bb53164a1eee87ab027
osm1892 <osm1892@sungkyul.ac.kr>
2022-05-04 00:48:07 +0900

    wip

--- chess.py
+++ chess.py
@@ -124,4 +124,9 @@ class ChessGame(Game):
 
 
 if __name__ == '__main__':
+    n: int = int(input("체스판의 크기를 입력해주세요: "))
+
+    print("n * n 체스판에 넣을 점수를 입력해주세요")
+    board: List[List[int]] = [list(map(int, input(f"{i}번째 줄: "))) for i in range(n)]
+
     print(f"플레이어 A와 B의 최대 점수차이는 {play_game(ChessGame()).utility()}점 입니다.")
--- main.py
+++
@@ -1,8 +0,0 @@
-from typing import *
-
-
-def main():
-    n: int = int(input("체스판의 크기를 입력해주세요: "))
-
-    print("n * n 체스판에 넣을 점수를 입력해주세요")
-    board: List[List[int]] = [list(map(int, input(f"{i}번째 줄: "))) for i in range(n)]
